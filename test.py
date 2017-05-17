de9429f085354d8ac9be9b9cb5cfa275efa0023c
dagbjoern <dag.hering@udo.edu>
2017-05-17 22:36:29 +0200

    erste Versuche

---
+++ test.py
@@ -0,0 +1,34 @@
+import numpy as np
+import numpy.linalg as linalg
+from sympy import *
+
+
+def Hamilton(e,J):
+    H=np.diag(e)
+    H[0,len(e)-1]=J
+    H[len(e)-1,0]=J
+    a=J
+    print('a',a)
+    for index, value in enumerate(e):
+            if(index < len(e)-1):
+                H[index+1,index]=H[0,len(e)-1]
+                H[index,index+1]=J
+    return H
+
+def eigenwerte(M):
+    eigenValues,eigenVectors = linalg.eig(M)
+    idx = eigenValues.argsort()[::-1]
+    print(idx)
+    eigenValues = eigenValues[idx]
+    eigenVectors = eigenVectors[:,idx]
+    return eigenValues, eigenVectors
+
+Test_H=Hamilton([0.1,0.1,0.1,0.1],1)
+Test_H_matrix=Matrix(Test_H)
+print(Test_H)
+
+
+print('eigenwerte und vektoren nach numpy',eigenwerte(Test_H))
+
+
+print('eigenwerte und eigenvektoren nach sympy',Test_H_matrix.eigenvects())
